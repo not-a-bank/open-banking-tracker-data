0e218d38c4f5579d426131430fc7ee462c29a41b
Gertjan De Wilde <info@gertjandewilde.com>
2019-03-13 03:19:15 +0100

    Fix validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,7 @@
-'use strict';
+'use strict'
 
 const Promise = require('bluebird')
 const fs = require('fs')
-const readFile = Promise.promisify(fs.readFile)
 const { getFiles, getFile } = require('./utils')
 const Ajv = require('ajv')
 const ajv = Ajv({ allErrors: true })
@@ -10,28 +9,39 @@ ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'))
 var validate = ajv.compile(require('../schema.json'))
 var validData = true
 
-function asyncFunction(file, cb) {
+function asyncFunction (file, cb) {
   getFile(file)
     .then(e => {
-      const valid = validate(JSON.parse(e))
-      if (!valid) {
-        console.log(file + ': ')
-        console.log(validate.errors)
+      try {
+        const json = JSON.parse(e)
+        const valid = validate(json)
+        if (!valid) {
+          console.log(file + ': ')
+          console.log(validate.errors)
+          validData = false
+        }
+        cb()
+      } catch (e) {
         validData = false
+        console.error('Failed processing: ' + file)
+        process.exitCode = 1
       }
-      cb()
     })
-    .catch(e => console.error(e))
+    .catch(e => {
+      validData = false
+      console.error('Failed processing: ' + file)
+      process.exitCode = 1
+    })
 }
 
 let requests = getFiles('data').map((fileName) => {
-  return new Promise((resolve) => { 
+  return new Promise((resolve) => {
     asyncFunction(fileName, resolve)
-    const ext = fileName.split('.').pop();
+    const ext = fileName.split('.').pop()
     if (ext !== 'json') {
       throw `"${ext}" must be saved with a JSON extension`
     }
-  });
+  })
 })
 
 Promise.all(requests).then(() => {
